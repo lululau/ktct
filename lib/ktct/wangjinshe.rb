7e181e1c3512dc6d58c7f95fd71b7d03fa383908
Liu Xiang <liuxiang@ktjr.com>
2019-05-10 11:03:33 +0800

    Add wangjinshe

--- lib/ktct/wangjinshe.rb
+++ lib/ktct/wangjinshe.rb
@@ -2,10 +2,41 @@ require 'thor'
 
 module Ktct
   class Wangjinshe < ::Thor
+    include Ktct::Config
+    include Ktct::Cryptable
 
-    desc '', ''
-    def hello
-      p 'hello'
+    no_commands do
+      alias_method :original_decrypt, :decrypt
+    end
+
+    desc 'encrypt [DATA]', "Encrypt data using Wangjinshe's public key, DATA can also be read from STDIN"
+    def encrypt(data = nil)
+      reset_key(config['server_public_key_path'])
+      puts super(data || STDIN.read)
+    end
+
+    desc 'decrypt [DATA]', "Decrypt data using client's private key, DATA can also be read from STDIN"
+    def decrypt(data = nil)
+      reset_key(config['client_private_key_path'])
+      puts super(data || STDIN.read)
+    end
+
+    desc 'sign [DATA]', "Sign data using client's private key, DATA can also be read from STDIN"
+    def sign(data = nil)
+      reset_key(config['client_private_key_path'])
+      puts super(data || STDIN)
+    end
+
+    desc 'verify <SIGNATURE> [DATA]', "Verify signature using Wangjingshe's public key, DATA can also be read from STDIN"
+    def verify(signature, data = nil)
+      reset_key(config['server_public_key_path'])
+      puts super(signature, data || STDIN.read)
+    end
+
+    no_commands do
+      def config
+        super['wjs']
+      end
     end
   end
 end
