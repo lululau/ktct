6ab947c5d071530ed325f42db3ebe6b74e119982
Liu Xiang <liuxiang@ktjr.com>
2019-05-10 12:36:06 +0800

    Fix bug

--- lib/ktct/wangjinshe.rb
+++ lib/ktct/wangjinshe.rb
@@ -1,4 +1,5 @@
 require 'thor'
+require 'open-uri'
 
 module Ktct
   class Wangjinshe < ::Thor
@@ -12,30 +13,34 @@ module Ktct
     desc 'encrypt [DATA]', "Encrypt data using Wangjinshe's public key, DATA can also be read from STDIN"
     def encrypt(data = nil)
       reset_key(config['server_public_key_path'])
-      puts super(data || STDIN.read)
+      puts URI::encode_www_form_component(Base64.strict_encode64(public_encrypt(data || STDIN.read)))
     end
 
     desc 'decrypt [DATA]', "Decrypt data using client's private key, DATA can also be read from STDIN"
     def decrypt(data = nil)
       reset_key(config['client_private_key_path'])
-      puts super(data || STDIN.read)
+      puts private_decrypt(Base64.strict_decode64(data || STDIN.read))
     end
 
     desc 'sign [DATA]', "Sign data using client's private key, DATA can also be read from STDIN"
     def sign(data = nil)
       reset_key(config['client_private_key_path'])
-      puts super(data || STDIN)
+      puts URI::encode_www_form_component(Base64.strict_encode64(key.sign('sha1', data || STDIN.read)))
     end
 
-    desc 'verify <SIGNATURE> [DATA]', "Verify signature using Wangjingshe's public key, DATA can also be read from STDIN"
-    def verify(signature, data = nil)
-      reset_key(config['server_public_key_path'])
-      puts super(signature, data || STDIN.read)
-    end
+    # desc 'verify <SIGNATURE> [DATA]', "Verify signature using Wangjinshe's public key, DATA can also be read from STDIN"
+    # def verify(signature, data = nil)
+    #   reset_key(config['server_public_key_path'])
+    #   puts key.verify('sha1', Base64.strict_decode64(signature), data || STDIN.read)
+    # end
 
     no_commands do
       def config
         super['wjs']
+      end
+
+      def reset_key(key_file)
+        @key = OpenSSL::PKey::RSA.new(IO.read(File.expand_path(key_file)))
       end
     end
   end
